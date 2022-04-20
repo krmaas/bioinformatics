220538dd981641cc12d895f7e723ca239898ac2a
Kendra Maas <kendra.r.maas@gmail.com>
2022-04-20 15:15:51 -0400

    Update mothur.bash
    update for 1.47

--- mothur.bash
+++ mothur.bash
@@ -24,13 +24,18 @@ cd $PROJECTNAME
 
 mv fastq/fileList.paired.file $PROJECTNAME.file
 
-module load mothur/1.38.1
+module load mothur/1.47
 mothur "#make.file(inputdir=fastq, type=gz);"
 
-module load mothur/1.39.4
+module load mothur/1.47
 
-mothur "#make.contigs(file=$PROJECTNAME.file); 
-make.contigs(processors=32, file=current); 
+#!bash
+PROJECTNAME=$1
+
+
+
+
+mothur "#make.contigs(file=$PROJECTNAME.file, processors=32); 
 summary.seqs(fasta=current); 
 screen.seqs(fasta=current, group=current, summary=current, maxambig=0, maxlength=275); 
 summary.seqs(fasta=current); 
@@ -45,7 +50,7 @@ summary.seqs(fasta=current, count=current);
 pre.cluster(fasta=current, diffs=2, count=current); 
 summary.seqs(fasta=current, count=current); 
 chimera.vsearch(fasta=current, count=current, dereplicate=t); 
-remove.seqs(fasta=current, accnos=current, count=current); 
+remove.seqs(fasta=current, accnos=current); 
 summary.seqs(fasta=current, count=current); 
 classify.seqs(fasta=current, count=current, reference=silva.nr_v119.v4.align, taxonomy=silva.nr_v119.tax, cutoff=80); 
 remove.lineage(fasta=current, count=current, taxonomy=current, taxon=Chloroplast-Mitochondria-unknown-Archaea-Eukaryota); 
@@ -59,7 +64,10 @@ get.oturep(fasta=current, count=current, list=current, method=abundance);
 count.groups(shared=current); 
 summary.single(shared=current, calc=nseqs-sobs-coverage-shannon-shannoneven-invsimpson, subsample=10000); 
 dist.shared(shared=current, calc=braycurtis-jest-thetayc, subsample=10000); 
-sub.sample(taxonomy=$PROJECTNAME.trim.contigs.good.unique.good.filter.precluster.pick.nr_v119.wang.pick.taxonomy, count=$PROJECTNAME.trim.contigs.good.unique.good.filter.precluster.denovo.vsearch.pick.pick.pick.count_table, list=$PROJECTNAME.trim.contigs.good.unique.good.filter.precluster.pick.pick.opti_mcc.list, size=10000, persample=true, label=0.03); 
-summary.tax(taxonomy=current, count=current); 
-system(mkdir send); system(cp *shared send); system(cp *cons.tax* send); system(cp *pick.tax.summary send); system(cp *pick.subsample.tax.summary send); system(cp *.rep.fasta send); system(cp *lt.ave.dist send); system(cp *groups.ave-std.summary send); system(cp mothur.batch send); system(cp mothur.*.logfile send);"
+sub.sample(count=$PROJECTNAME.trim.contigs.good.unique.good.filter.precluster.denovo.vsearch.pick.pick.count_table, shared=current, list=$PROJECTNAME.trim.contigs.good.unique.good.filter.precluster.pick.pick.opti_mcc.list, size=10000, persample=true, label=0.03); 
+sub.sample(taxonomy=$PROJECTNAME.trim.contigs.good.unique.good.filter.precluster.pick.nr_v119.wang.pick.taxonomy, count=$PROJECTNAME.trim.contigs.good.unique.good.filter.precluster.denovo.vsearch.pick.pick.count_table, list=$PROJECTNAME.trim.contigs.good.unique.good.filter.precluster.pick.pick.opti_mcc.list, size=10000, persample=true, label=0.03); 
+summary.tax(taxonomy=current, count=current); system(mkdir send); 
+system(cp *shared send); system(cp *cons.tax* send); system(cp *pick.tax.summary send); system(cp *pick.subsample.tax.summary send); system(cp *.rep.fasta send); system(cp *lt.ave.dist send); system(cp *groups.ave-std.summary send); system(cp mothur.bash send); system(cp mothur.*.logfile send);"
+
+
 
